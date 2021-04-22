dc6fd92cb2890c3080cc8f284e1f16cd3716eb96
Nikita Kondrashov <nickita.kondraschov@yandex.ru>
2021-04-22 17:56:13 +0300

    commit 2

--- django_testing/tests/students/test_courses_api.py
+++ django_testing/tests/students/test_courses_api.py
@@ -2,16 +2,15 @@
 #     assert False, "Just test example"
 import pytest
 from django.urls import reverse
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
 
 
 @pytest.mark.django_db
 def test_one_course(api_client,course_factory):
     course = course_factory()
-    url = reverse('courses-detail', args=course.id)
+    url = reverse('courses-detail', args=(course.id, ))
     resp = api_client.get(url)
     assert resp.status_code == HTTP_200_OK
-    assert resp.json()['id'] == course.id
 
 
 @pytest.mark.django_db
@@ -21,3 +20,53 @@ def test_list_course(api_client,course_factory):
     resp = api_client.get(url)
     assert resp.status_code == HTTP_200_OK
 
+
+@pytest.mark.django_db
+def test_id_course(api_client,course_factory):
+    course = course_factory(_quantity=4)
+    id_course = course[1].id
+    url = reverse('courses-list')
+    resp = api_client.get(url)
+    resp_json = resp.json()
+    resp_ids = {r['id'] for r in resp_json}
+    assert resp.status_code == HTTP_200_OK
+    assert id_course in resp_ids
+
+@pytest.mark.django_db
+def test_name_course(api_client,course_factory):
+    course = course_factory(_quantity=4)
+    name_course = course[1].name
+    url = reverse('courses-list')
+    resp = api_client.get(url)
+    resp_json = resp.json()
+    resp_ids = {r['name'] for r in resp_json}
+    assert resp.status_code == HTTP_200_OK
+    assert name_course in resp_ids
+
+@pytest.mark.django_db
+def test_create_course(api_client,course_factory):
+    url = reverse('courses-list')
+    course = {'name': 'test',
+              'students': [],
+    }
+    resp = api_client.post(url, course, fotmat='json')
+    assert resp.status_code == HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_path_course(api_client,course_factory):
+    course = course_factory()
+    url = reverse('courses-detail', args=(course.id, ))
+    course = {'name': 'test',
+              }
+    resp = api_client.patch(url, fotmat='json',content_type='application/json')
+    assert resp.status_code == HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_delete_course(api_client,course_factory):
+    course = course_factory()
+    url = reverse('courses-detail', args=(course.id, ))
+    resp = api_client.delete(url, course, fotmat='json',content_type='application/json')
+    assert resp.status_code == HTTP_204_NO_CONTENT
+    
